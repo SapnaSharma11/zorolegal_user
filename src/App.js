d43f57d652831c0e06fe8acf31897a48d4f7833d
sapnaRidhi <sapnaridhi11@gmail.com>
2023-01-24 16:18:17 +0530

    add home page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signup from "./pages/SignUp/signup.jsx";
+import Header from "./components/Header"
+import { Home } from "./pages/Home/Home";
+
 
 
 
@@ -10,9 +13,10 @@ function App() {
   return (
     <>
       <BrowserRouter>
-
+        <Header></Header>
         <Routes>
           <Route path="/" element={<Signup />}/>
+          <Route path="/home" element={<Home />}/>
    
 
           </Routes>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Home.css'
+
+export function Home(){
+    return(
+        <>
+        <h2>Your service section</h2>
+        <h3>Your testimonial section</h3>
+        </>
+    )
+}
